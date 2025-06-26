5486e41c5e9ec49aee3f1689ea9f39bfd025ec52
tjf9549 <tjf9549er@gmail.com>
2025-06-26 02:38:55 -0500

    remove dead reference

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
 export { default as useModal } from './useModal';
-export { useDraggable, useDroppable, useDragState } from './useDragAndDrop';
+export { useDraggable, useDroppable } from './useDragAndDrop';
 export type { DragItem, DragState, UseDraggableOptions, UseDroppableOptions } from './useDragAndDrop';
